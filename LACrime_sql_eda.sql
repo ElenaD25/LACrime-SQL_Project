585fa1cd7e99b0cdd39b10c7c83006be1cf57f5a
Luchiana Dumitrescu <luchiana.dumitrescu@nbd.ro>
2024-03-06 23:05:00 +0200

    added the number of reported crimes per month, number of cases by status,  area least safe by number of cases reported,  most crimes per year, most common crimes locations

--- LACrime_sql_eda.sql
+++ LACrime_sql_eda.sql
@@ -128,27 +128,32 @@ order by count(id),year(occured_date) desc
 
 --- time of day
 
-select * from reportedcrime;
-
---select sum(x.reported_cases), substring(x.occured_time,1,1) 
---from (
---select count(*) as reported_cases, occured_time
---from reportedcrime
---where occured_time like '10%'
---group by occured_time
---) x
---group by substring(x.occured_time,1,1) 
-
-
 select distinct format(try_convert(datetime,trim(occured_time)),'h tt', 'en-US') as occured_hour,
 count(*) over (partition by format(try_convert(datetime,trim(occured_time)),'h tt', 'en-US')) as no_of_reported_cases
-from reportedcrime;
+from reportedcrime
+order by format(try_convert(datetime,trim(occured_time)),'h tt', 'en-US')
 
+
+--- month
+
+--without a specific year
+
+select count(id) as no_of_reported_cases, datename(month, occured_date) as month_reported
+from reportedcrime
+--where year(occured_date) = 2023 
+group by datename(month, occured_date)
+order by count(id) desc
+
+
+--for a specific year
+select count(id) as no_of_reported_cases, datename(month, occured_date) as month_reported
+from reportedcrime
+where year(occured_date) = 2023 
+group by datename(month, occured_date)
+order by count(id) desc
 
 
 --What are the statuses of the reported cases?
-
-select * from reportedcrime;
 
 select status_desc, count(*) as no_of_reported_cases, format(count(*) * 100.0 / (select count(*) from reportedcrime),'N2') as percentage_by_status
 from reportedcrime
@@ -156,54 +161,44 @@ group by status_desc
 order by count(*) desc
 
 
+--Which areas are considered the least safe by the number of reported cases
+
+select top 10 area, count(id) as no_of_reported_cases
+from reportedcrime
+group by area
+order by count(id) desc;
 
 
---Which areas are considered the least safe?
--- On which weekday do we observe the highest number of reported cases?
---- most common crimes per year --https://www.kaggle.com/code/amaninaman/map-visualisation-preprocessing-bar-charts#(3)-INTERACTIVE-MAP-OF-CRIMES-BY-YEAR
+--- most common crimes per year 
+
+WITH RankedCrimes AS (
+    SELECT
+        year(occured_date) AS year_occurred,
+        crime_code,
+        crime_desc,
+        COUNT(id) AS no_of_reported_cases,
+        RANK() OVER (PARTITION BY year(occured_date) ORDER BY COUNT(id) DESC) AS crime_rank
+    FROM
+        reportedcrime
+    GROUP BY
+        year(occured_date),
+        crime_code,
+        crime_desc
+)
+SELECT
+    year_occurred,
+    crime_code,
+    crime_desc,
+    no_of_reported_cases
+FROM
+    RankedCrimes
+WHERE
+    crime_rank = 1;
 
 
+-- what are the most common crime location descriptions (Crime_Location_Desc)?
 
-Temporal Analysis:
-
-What is the overall trend in reported cases over time?
-Which month experiences the highest number of reported cases?
-Are there any significant patterns in the day of the week or hour of the day when incidents occur?
-
-
-Geospatial Analysis:
-Which areas (Area_Code, Area, District_No) have the highest and lowest crime rates?
-Can you identify specific locations (Location, Lat, Lon) with a concentration of reported cases?
-Crime Types:
-
-What are the top three most frequently reported crime types (Crime_Desc)?
-How does the distribution of crime types vary across different areas?
-Demographic Analysis:
-
-What is the age distribution of victims?
-Can you identify any patterns related to the gender and race of victims in reported cases?
-
-Weapon Usage:
-Which weapons (Weapon_Desc) are most commonly associated with reported cases?
-Are there specific crime types where weapons are frequently used?
-Status Analysis:
-
-What is the distribution of case statuses (Status_Desc)?
-Are there differences in the resolution status of cases across different crime types?
-Victim Characteristics:
-
-Is there a correlation between victim age and the severity of the crime (e.g., using Crime_Code)?
-What are the most common crime locations (Crime_Location_Desc) for different victim demographics?
-Time to Resolution:
-
-What is the average time to resolve reported cases?
-Are there differences in resolution time based on crime types or areas?
-Weekday Analysis:
-
-On which weekday do you observe the highest number of reported cases?
-Are there variations in the distribution of incidents based on the day of the week?
-Location Descriptions:
-
-What are the most common crime location descriptions (Crime_Location_Desc)?
-Can you identify any patterns in reported incidents based on location descriptions?
+select top 10 crime_location_code, crime_location_desc, count(id) as no_of_reported_cases, rank() over (order by count(id) desc) as case_rank
+from reportedcrime
+group by crime_location_code, crime_location_desc
 
